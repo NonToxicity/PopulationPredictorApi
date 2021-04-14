209cc70a9d9297346de137ac6c3fa1ed980491a5
Mert Kaya <47145541+mertkayacs@users.noreply.github.com>
2021-04-15 00:05:44 +0300

    .

---
+++ app.py
@@ -0,0 +1,11 @@
+import flask
+from flask import request
+import pandas as pd
+
+
+app = flask.Flask(__name__)
+
+@app.route('/',methods = ['GET'])
+def home():
+    arg = request.args['arg']
+    return 'Hello World'
